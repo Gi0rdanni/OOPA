eb2358a900e144d791bdb3369be8e100d0b53788
gi0rdanni <135524825+Gi0rdanni@users.noreply.github.com>
2025-11-05 10:24:11 -0300

    Refactor item UI handling into separate functions

--- 01/app/views/item_view.py
+++ 01/app/views/item_view.py
@@ -7,23 +7,28 @@ def items_to_dataframe(items: list["Item"]) -> None:
     items_data = [vars(item) for item in items]
     streamlit.dataframe(items_data)
 
+def create_item_ui():
+    description = streamlit.text_area("Description")
+    quantity = streamlit.number_input("Quantity", min_value=0, step=1)
+
+    if streamlit.button("Submit"):
+        item_controller = ItemController()
+        data = {"description": description, "quantity": quantity}
+        item = item_controller.create(**data)
+        streamlit.success("Item created successfully!")
+        items_to_dataframe([item])
+
+def read_items_ui():
+    item_controller = ItemController()
+    items = item_controller.list_all()
+    items_to_dataframe(items)
+
 def main():
     menu_option = streamlit.sidebar.selectbox(
         "Select an operation:",
         ["Create", "Read"]
     )
 
-    if menu_option == "Create":
-        description = streamlit.text_area("Description")
-        quantity = streamlit.number_input("Quantitty", min_value = 0, step = 1)
-
-        if streamlit.button("Submit"):
-            item_controller = ItemController()
-            data = {"description": description, "quantity": quantity}
-            item = item_controller.create(**data)
-            items_to_dataframe([item])
-
-    elif menu_option == "Read":
-        item_controller = ItemController()
-        items = item_controller.list_all()
-        items_to_dataframe(items)
+    match menu_option:
+        case "Create": create_item_ui()
+        case "Read": read_items_ui()
